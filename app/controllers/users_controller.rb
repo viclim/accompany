8e6aeb5c0e6cc433ddd3ae9f2ec37f013cb7a15a
Victor Lim <victor@rrsoft.co>
2015-12-01 19:54:00 +0800

    add user policy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.json
   def index
-    @users = User.all
+    @users = policy_scope(User)
   end
 
   # GET /users/1
---
+++ app/policies/user_policy.rb
@@ -0,0 +1,34 @@
+class UserPolicy < ApplicationPolicy
+  class Scope
+    attr_reader :user, :scope
+
+    def initialize(user, scope)
+      @user = user
+      @scope = scope
+    end
+
+    def resolve
+      if user.role?(:boss)
+        scope.all
+      elsif user.role(:head)
+        scope.where(department_id: user.department_id)
+      else
+        scope.where(id: user.id)
+      end
+    end
+  end
+
+  def initialize(user, item)
+
+  end
+
+  def create?
+  end
+
+  def update?
+  end
+
+  def destroy?
+
+  end
+end
