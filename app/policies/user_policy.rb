bc6a9828005c008f975971fb5f3887d38bea3d1c
Victor Lim <victor@rrsoft.co>
2015-12-01 20:14:07 +0800

    add hr

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -8,7 +8,7 @@ class UserPolicy < ApplicationPolicy
     end
 
     def resolve
-      if user.role?(:boss)
+      if user.role?(:boss) || user.department.try(:name) == 'HR'
         scope.all
       elsif user.role?(:head)
         scope.where(department_id: user.department_id)
@@ -23,6 +23,7 @@ class UserPolicy < ApplicationPolicy
   end
 
   def create?
+    user.role?(:boss) || (user.role?(:head) && user.department == item.department)
   end
 
   def update?
@@ -30,5 +31,13 @@ class UserPolicy < ApplicationPolicy
 
   def destroy?
 
+  end
+
+  private
+
+  def modify?
+    user.role?(:boss) ||
+      (user.role?(:head) && user.department == item.department) ||
+      user.department.name == 'HR'
   end
 end
